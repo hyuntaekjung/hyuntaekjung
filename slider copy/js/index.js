730079725140f969681513fc0ac7e1ccf7f562b1
hyuntaekjung <gus104202@naver.com>
2022-10-31 22:19:38 +0900

    20221031

--- slider copy/js/index.js
+++ slider copy/js/index.js
@@ -2,12 +2,6 @@ let itemList = document.querySelectorAll(".item");
 let btnList = document.querySelectorAll(".btns li button");
 let wrapper = document.querySelector(".wrapper");
 
-//  슬라이드 아이템 색상 추가
-itemList[0].style.backgroundColor = "red";
-itemList[1].style.backgroundColor = "blue";
-itemList[2].style.backgroundColor = "yellow";
-
-// 버튼 클릭시 슬라이드 움직이기
 btnList.forEach(function (btn, idx) {
   btn.addEventListener("click", function () {
     wrapper.style.transform = `translateX(${idx * -100}vw)`;
